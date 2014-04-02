844278ac83a8fe3bfcab4f13134552bf43b3b225
Andy Gardner <andy.gardner@fundamental.net>
2014-04-02 17:12:48 +0200

    General refinements and cleanup

--- source/tweet.js
+++ source/tweet.js
@@ -1,33 +1,17 @@
-function TwootCtrl($scope) {
-  $scope.twoots = [
-    {text:'learn angular', done:true},
-    {text:'build an angular app', done:false}];
- 
+var twootApp = angular.module('twootApp', []);
+
+twootApp.controller('TwootCtrl', function ($scope, $http) {
+  $scope.twootText = '';
+
+  $http.get('/api/stream.json').success(function(data) {
+    $scope.twoots = data;
+  });
+
   $scope.addTwoot = function() {
     $scope.twoots.push({text:$scope.twootText, done:false});
     $scope.twootText = '';
   };
   
-  $scope.validateTwoot = function() {
-    //$scope.twoots.push({text:$scope.twootText, done:false});
-    //$scope.twootText = '';
-    //140 - len($scope.twootText);
-    //$scope.twootText = '';
-    //$scope.twootTextChars = 140 - $scope.twootText.length();
-    
-    // var text = $scope.twootText;
-    // console.log('running validateTwoot...')
-    // if (text.length > 140){
-    //   //document.getElementById('twoot-box').innerHTML()
-    //   $scope.twootText = [text.slice(0, 140), "<strong>", text.slice(140), "</strong>"].join('');
-    // }
-    //$scope.twootText = document.getElementById('twoot-box').innerText;
-    
-    //console.log('twoot-box: '+getCaretCharacterOffsetWithin(document.getElementById('twoot-box')));
-    //$scope.invalid = ($scope.twootText.length <= 140);
-    //console.log($scope.invalid);
-  }; 
-
   $scope.remaining = function() {
     var count = 0;
     angular.forEach($scope.twoots, function(twoot) {
@@ -43,40 +27,4 @@ function TwootCtrl($scope) {
       if (!twoot.done) $scope.twoots.push(twoot);
     });
   };
-}
-
-
-angular.module('twoot', []).directive('contenteditable', function() {
-  return {
-    require: 'ngModel',
-    link: function(scope, elm, attrs, ctrl) {
-      // view -> model
-      elm.on('keypress keyup blur', update);
-      // elm.on('keyup', update);
-      // elm.on('blur', update);
-
-      function update() {
-        scope.$apply(function() {
-          ctrl.$setViewValue(elm.text());
-
-          //console.log('twoot-box: '+getCaretCharacterOffsetWithin(document.getElementById('twoot-box')));
-
-          // var temp = elm.text();
-          // elm.html([temp.slice(0, 140), "<strong>", temp.slice(140), "</strong>"].join(''));
-          // console.log('twoot-box: '+getCaretCharacterOffsetWithin(document.getElementById('twoot-box')));
-          // ctrl.$setViewValue(temp);
-        });
-      }
- 
-      // model -> view
-      ctrl.$render = function() {
-        elm.html(ctrl.$viewValue);
-      };
- 
-      // load init value from DOM
-      ctrl.$setViewValue(elm.text());
-    }
-  };
 });
-
-
