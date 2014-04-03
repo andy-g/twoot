4ac6c5f5da8210c5e9a841c359e3774c75324dce
Andy Gardner <andy.gardner@fundamental.net>
2014-04-03 15:28:28 +0200

    Adding redis for storage

--- source/app.js
+++ source/app.js
@@ -1,5 +1,15 @@
 var	express = require('express');
 
+var redis = require("redis"),
+	client = redis.createClient();
+client.retry_max_delay = 5000;
+
+client.on("error", function (err) {
+	console.log("Error " + err);
+});
+
+const user = 'user1234';
+
 var app = express();
 app.use(express.urlencoded());
 app.use(express.json());
@@ -9,22 +19,35 @@ app.use(express.static(__dirname + '/static'));
 app.get('/api/twoots/:twootId?', twoots);
 app.post('/api/twoots/:twootId?', saveTwoot);
 
-var stub = [
-	{"text":"My first tweet!!!", "created":1396444920000},
-	{"text":"Oh so original", "created":1396445220000}
-]
 
 function twoots(req, res) {
 	var twootId = req.param('twootId');
-	res.json(stub);
+	client.lrange(user, 0, 50, function(err, arr){ 
+		if (!arr)
+			res.json([]);
+		else{
+			var parsed_arr = arr.map(function(item){
+				return JSON.parse(item);
+			})
+			res.json(parsed_arr)
+		}
+	});
 };
 
 
 function saveTwoot(req, res) {
 	var twoot = req.body;
+	twoot.status = 1;
 	console.log(twoot);
-	stub.push(twoot);
+	client.lpush(user, JSON.stringify(twoot), function(error, data){
+		if (!!error){
+			console.dir(error);
+			res.json(500, {"error" : "There was an error sending your tweet, please retry later."});
+		}else{
+			console.dir(data);
 	res.json(twoot);
+		}
+	});
 };
 
 app.listen(3000);
